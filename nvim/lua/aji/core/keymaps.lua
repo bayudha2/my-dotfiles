d4ce38c0e8d3c9abcb61f7c0fd7c3d2848818f7a
bayudha2 <bayudha.bd@gmail.com>
2024-12-24 16:21:07 +0700

    feat: update golang run all available test command
    - update keymaps (add new open terminal mode shortcut)

--- nvim/lua/aji/core/keymaps.lua
+++ nvim/lua/aji/core/keymaps.lua
@@ -5,12 +5,12 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search hightlights" })
 
 -- vertical movement
-keymap.set("n", "J", "5j", { desc = "Go down for 5 row"})
-keymap.set("v", "J", "5j", { desc = "Go down for 5 row on visual mode"})
+keymap.set("n", "J", "5j", { desc = "Go down for 5 row" })
+keymap.set("v", "J", "5j", { desc = "Go down for 5 row on visual mode" })
 keymap.set("n", "K", "5k", { desc = "Go up for 5 row", remap = true })
 keymap.set("v", "K", "5k", { desc = "Go up for 5 row on visual mode", remap = true })
-keymap.set("n", "{", "{", { desc = "Go up to blank row and center view"})
-keymap.set("n", "}", "}", { desc = "Go down to blank row and center view"})
+keymap.set("n", "{", "{", { desc = "Go up to blank row and center view" })
+keymap.set("n", "}", "}", { desc = "Go down to blank row and center view" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
@@ -21,6 +21,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- Make splits equal size
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- Close current split
+keymap.set("n", "<leader>sn", "<cmd>vnew | terminal<CR>", { desc = "Open new terminal in split" }) -- Open new terminal mode in spit vim
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- Open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- Close current tab
